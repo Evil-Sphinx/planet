31689e84d2366ab6889f362e54b5a9ef284f9531
filco <filco9617@gmail.com>
2025-01-15 19:03:48 +0800

    Sidebar

---
+++ src/layouts/sidebar/index.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+
+import { Layout, Menu } from 'antd';
+
+import sideMenu from 'src/configs/sidemenu';
+
+const { Sider } = Layout;
+const { SubMenu } = Menu;
+
+const Sidebar = () => {
+  const [collapsed] = useState(false);
+  return (
+    <Sider collapsed={collapsed}>
+      <Menu
+        theme="dark"
+        //   selectedKeys={[state.highlight || selectedMatched[selectedMatched.length - 1]]}
+        mode="inline"
+        //   defaultOpenKeys={this.getDefaultOpenKeys()}
+        //   onOpenChange={this.handleFooter}
+      >
+        {sideMenu.map((item) => {
+          const { key, name, icon, url, child, shortName } = item;
+          if (Array.isArray(child) && child.length) {
+            return (
+              <SubMenu
+                key={key}
+                title={
+                  <span className="sider__menu__title">
+                    {/* <i className={iconClass} /> */}
+                    <span>{collapsed ? shortName : name}</span>
+                  </span>
+                }>
+                {child.map((childItem) => {
+                  return (
+                    <Menu.Item key={`${childItem.key}`}>
+                      {
+                        <a href={childItem.url} className="sider__menu__child">
+                          <span>{childItem.name}</span>
+                        </a>
+                      }
+                    </Menu.Item>
+                  );
+                })}
+              </SubMenu>
+            );
+          }
+          return (
+            <Menu.Item key={key} title={name}>
+              <a href={url} className="sider__menu__title">
+                <i className={`icon-biomart-admin ${icon}`} />
+                <span>{collapsed ? shortName : name}</span>
+              </a>
+            </Menu.Item>
+          );
+        })}
+      </Menu>
+    </Sider>
+  );
+};
+
+export default Sidebar;
--- src/layouts/wrapper.js
+++ src/layouts/wrapper.js
@@ -1,8 +1,16 @@
+import { Layout } from 'antd';
 import React from 'react';
+import Sidebar from './sidebar';
 
+const { Content } = Layout;
 const Wrapper = (props) => {
   const { children } = props;
-  return <>{React.Children.map(children, (child) => React.cloneElement(child))}</>;
+  return (
+    <Layout>
+      <Sidebar />
+      <Content>{React.Children.map(children, (child) => React.cloneElement(child))}</Content>
+    </Layout>
+  );
 };
 
 export default Wrapper;

8c58f00b8f457eea42909de6b34aab6f1c34790b
filco <filco9617@gmail.com>
2025-01-16 19:10:07 +0800

    QQSG activity

--- src/views/activity/index.js
+++ src/views/activity/index.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { activity } from '@/constants/activity';
+import classNames from 'classnames';
+import './index.css';
 
 const LEVEL_1 = 180;
 const LEVEL_2 = 300;
 const LEVEL_3 = 350;
 
 const Activity = () => {
+  const [playerLevel] = useState(90);
+  let score = 0;
+
   return (
-    <>
+    <div>
       {activity
-        .sort((a, b) => a.value - b.value)
+        .sort((a, b) => a.difficulty - b.difficulty)
         .map((item, index) => {
-          return <div key={index}>{item.activityName}</div>;
+          const currentScore = (score += item.point[playerLevel]);
+          return (
+            <div
+              className={classNames({
+                cold: currentScore < LEVEL_1,
+                ordinary: currentScore >= LEVEL_1 && currentScore < LEVEL_2,
+                hot: currentScore >= LEVEL_2 && currentScore < LEVEL_3,
+                excellent: currentScore >= LEVEL_3
+              })}
+              key={index}>
+              {item.activityName}
+            </div>
+          );
         })}
-    </>
+    </div>
   );
 };
 
